c3fc71eef018530c4812b7b2de979049eb75ff36
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2016-11-13 10:49:30 +0700

    #time 1h #comment Import FormsModule untuk penggunaan "two ways data binding". + Form untuk role management

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,21 +1,21 @@
 import { Component } from '@angular/core';
 
 export class RoleComponent {
-  id: number;
-  name: string;
-  icon: string;
-  desc: string;
-  status: string;
+    id: number;
+    name: string;
+    icon: string;
+    desc: string;
+    status: string;
 }
 
 const ROLES: RoleComponent[] = [
-  { id: 1, name: 'Admin', icon: 'spy', desc:'Description admin here', status:'green'},
-  { id: 2, name: 'User', icon: 'student', desc:'Description user here', status:'green'},
+    { id: 1, name: 'Admin', icon: 'spy', desc: 'Description admin here', status: 'green' },
+    { id: 2, name: 'User', icon: 'student', desc: 'Description user here', status: 'green' },
 ];
 
 @Component({
-  selector: 'my-app',
-  template: `
+    selector: 'my-app',
+    template: `
   <h1>{{title}}</h1>
   <h3>Roles</h3>
   <div class="ui cards">
@@ -38,29 +38,28 @@ const ROLES: RoleComponent[] = [
 
   <div *ngIf="selectedRole">
     <h3>Details</h3>
-    <div>
-      <label>ID: </label>{{selectedRole.id}}
-    </div>
-    <div>
-      <label>Name: </label>{{selectedRole.name}}
-    </div>
-    <div>
-      <label>Icon: </label>{{selectedRole.icon}}
-    </div>
-    <div>
-      <label>Desc: </label>{{selectedRole.desc}}
-    </div>
+    <form class="ui form">
+      <div class="field">
+      <label>ID: </label>
+      <input type="text" [(ngModel)]="selectedRole.id" name="id" placeholder="ID"/>
+      </div>
+      <div class="field">
+      <label>Name: </label>
+      <input type="text" [(ngModel)]="selectedRole.name" name="name" placeholder="Name"/>
+      </div>
+    </form>
   </div>
+
   `
 })
 
 export class AppComponent {
-  title = 'Alumni Tracker';
-  roles = ROLES;
-  selectedRole: RoleComponent;
+    title = 'Alumni Tracker';
+    roles = ROLES;
+    selectedRole: RoleComponent;
 
-  onSelect(role: RoleComponent): void {
-    console.log("Click", role);
-    this.selectedRole = role;
-  }
+    onSelect(role: RoleComponent): void {
+        console.log("Click", role);
+        this.selectedRole = role;
+    }
 }
