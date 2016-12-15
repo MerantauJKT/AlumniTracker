287531a8211e97a8b56e98f58ed676ceafb77622
ulil amri <apple@Apples-MacBook-Pro.local>
2016-12-15 07:22:23 +0700

    commit

--- app/app.component.js
+++ app/app.component.js
@@ -9,34 +9,32 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
+var USERS = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'Tour of Users';
+        this.users = USERS;
     }
+    AppComponent.prototype.onSelect = function (user) {
+        this.selectedUser = user;
+    };
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: `
-<div class="ui secondary pointing menu">
-  <a class="active item">
-    Home
-  </a>
-  <a class="item">
-    Messages
-  </a>
-  <a class="item">
-    Friends
-  </a>
-  <div class="right menu">
-    <a class="ui item">
-      Logout
-    </a>
-  </div>
-</div>
-<div class="ui segment">
-  <p></p>
-</div> `
+        template: "\n    \n<div class=\"ui grid\"> \n<div class=\"sixteen wide column\">\n<div class=\"ui secondary pointing menu\">\n  <a class=\" item\">\n    Home\n  </a>\n  <a class=\"active item\">\n    User Management\n  </a>\n  <div class=\"right menu\">\n    <a class=\"ui item\">\n      Logout\n    </a>\n  </div>\n</div>\n<div class=\"ui segment\">\n\n  <div class=\"ui grid\">\n  <div class=\"sixteen wide column\"><h1>{{title}}</h1>\n  <div class=\"ui three cards\">\n    <div *ngFor=\"let user of users\" class=\"fluid link card\" (click)=\"onSelect(user)\">\n          <div class=\"content\">\n            <div class=\"header\">\n              <h2 class=\"ui header\">\n                <i class=\"user icon\"></i>\n                <div class=\"content\">\n                  {{user.name}}\n                </div>\n              </h2>\n            </div>\n          </div>\n    </div>\n    </div>\n  <div class=\"sixteen wide column\"><my-user-detail [user]=\"selectedUser\"></my-user-detail></div>\n</div>\n</div>\n</div>\n</div>\n</div>\n"
     }),
     __metadata("design:paramtypes", [])
 ], AppComponent);
--- app/app.module.js
+++ app/app.module.js
@@ -10,7 +10,9 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
+var forms_1 = require("@angular/forms");
 var app_component_1 = require("./app.component");
+var user_detail_component_1 = require("./user-detail.component");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -18,8 +20,14 @@ var AppModule = (function () {
 }());
 AppModule = __decorate([
     core_1.NgModule({
-        imports: [platform_browser_1.BrowserModule],
-        declarations: [app_component_1.AppComponent],
+        imports: [
+            platform_browser_1.BrowserModule,
+            forms_1.FormsModule
+        ],
+        declarations: [
+            app_component_1.AppComponent,
+            user_detail_component_1.UserDetailComponent
+        ],
         bootstrap: [app_component_1.AppComponent]
     }),
     __metadata("design:paramtypes", [])
