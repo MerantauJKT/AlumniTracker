d4afdc4ce5efc65fefd9a8f166a8ea7f26df11c6
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2016-12-18 03:11:51 +0700

    #time 2h #comment Menambahkan halaman sign in/login

---
+++ app/signin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'signin',
+  templateUrl: 'signin.component.html'
+})
+
+export class SigninComponent{
+  constructor(){}
+}
